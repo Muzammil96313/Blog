1278e2963146c15262b88224edb8004339f25a23
Muzammil Hassan <muzammil.hassan.dev7@gmail.com>
2025-07-05 14:25:18 +0500

    new

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -44,7 +44,7 @@ const CommentForm = ({ postId, fetchComments }) => {
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
         disabled={isCommentButtonDisabled}
       >
-        {isCommentButtonDisabled ? "Adding..." : "Add Comment"}
+        {isCommentButtonDisabled ? "Adding...." : "Add Comment"}
       </button>
     </form>
   );
